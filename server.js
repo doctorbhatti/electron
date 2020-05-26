886c86a3c5d1a62b0c077675fa77f24bab13e8f7
Hassan Bhatti <dochassan12@outlook.com>
2020-05-26 08:43:48 +0500

    new

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
